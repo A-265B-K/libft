c660ead730bb69c45f425a73ff652b4f9aaa4532
Alyssa Kuijten <alkuijte@f0r2s7.codam.nl>
2023-10-23 17:53:37 +0200

    justm kaing sure

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,19 +6,12 @@
 /*   By: alkuijte <alkuijte@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2023/10/23 17:45:01 by alkuijte      #+#    #+#                 */
-/*   Updated: 2023/10/23 17:51:15 by alkuijte      ########   odam.nl         */
+/*   Updated: 2023/10/23 17:53:30 by alkuijte      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
-char	*ft_striteri(char const *s, char (*f)(unsigned int, char))
+void	*ft_striteri(char const *s, char (*f)(unsigned int, char))
 {
-	int		i;
-	char	*str;
-
-	i = 0;
-	str = (char *)malloc(sizeof(*str) * (ft_strlen(s) + 1));
-	while (s[i] != '\0')
-		str[i] = f(i, *(char *)(s + i));
-	str[i] = '\0';
-	return (str);
+	while (*s++ != '\0')
+		*s = f(i, *s);
 }

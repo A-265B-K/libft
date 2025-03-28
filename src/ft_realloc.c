dce26abed48a61ed09b7174f79542f57eafa8994
Alyssa Kuijten <alkuijte@f1r1s9.codam.nl>
2025-03-28 13:46:23 +0100

    fix

--- src/ft_realloc.c
+++ src/ft_realloc.c
@@ -6,31 +6,32 @@
 /*   By: alkuijte <alkuijte@student.codam.nl>         +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2025/03/28 11:24:44 by alkuijte      #+#    #+#                 */
-/*   Updated: 2025/03/28 11:33:00 by alkuijte      ########   odam.nl         */
+/*   Updated: 2025/03/28 13:32:34 by alkuijte      ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../libft.h"
 
-void	*ft_realloc(void *ptr, size_t new_size)
+void	*ft_realloc(void *ptr, size_t old_size, size_t new_size)
 {
 	void	*new_ptr;
-	size_t	old_size;
+	size_t	copy_size;
 
+	if (!ptr)
+		return (malloc(new_size));
 	if (new_size == 0)
 	{
 		free(ptr);
 		return (NULL);
 	}
-	if (!ptr)
-		return (malloc(new_size));
-	old_size = ft_strlen(ptr);
-	if (new_size <= old_size)
-		return (ptr);
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
 		return (NULL);
-	ft_memcpy(new_ptr, ptr, old_size);
+	if (old_size < new_size)
+		copy_size = old_size;
+	else
+		copy_size = new_size;
+	ft_memcpy(new_ptr, ptr, copy_size);
 	free(ptr);
 	return (new_ptr);
 }
